fcf777a1f376d8f174adad1cbd8c64976bd4f894
Yui Qi Tang <yqtang1222@gmail.com>
2019-06-19 14:40:31 +0800

    add comment for int slice foreach

--- slice/slice.go
+++ slice/slice.go
@@ -3,6 +3,7 @@ package slice
 // IntSlice a int slice
 type IntSlice []int
 
+// ForEach executes a provided function once for each int slice element
 func (i IntSlice) ForEach(f func(v int)) {
 	for _, v := range i {
 		f(v)
